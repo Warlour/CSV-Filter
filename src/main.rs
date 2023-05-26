2836354e54a11e3c49bfeec392e31df4b9306bb9
Mathias K. E. Sørensen <68961022+Warlour@users.noreply.github.com>
2023-05-27 00:46:22 +0200

    Positive prefix and no prefix not working as intended Negative prefix working as intended

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use clap::{Arg, App};
 
 fn main() {
-    let matches = App::new("CSV Filter")
+    let matches: clap::ArgMatches = App::new("CSV Filter")
         .arg(Arg::with_name("input")
             .short("i")
             .long("input")
@@ -32,19 +32,56 @@ fn main() {
 }
 
 fn read_csv_file(filename: &str, keywords: &str) -> Result<(), Box<dyn Error>> {
-    let file = File::open(filename)?;
-    let mut rdr = csv::Reader::from_reader(file);
+    let file: File = File::open(filename)?;
+    let mut rdr: csv::Reader<File> = csv::Reader::from_reader(file);
 
     let keyword_list: Vec<&str> = keywords.split(';').collect();
 
     for result in rdr.records() {
-        let record = result?;
-        let text = record.get(0).unwrap_or("");
+        let record: csv::StringRecord = result?;
+        let text: &str = record.get(0).unwrap_or("");
 
-        if keyword_list.iter().any(|&keyword| text.contains(keyword)) {
+        let mut is_match: bool = true;
+        let mut has_positive_requirement: bool = false;
+        let mut has_negative_requirement: bool = false;
+
+        for keyword in &keyword_list {
+            let (prefix, keyword) = parse_keyword(keyword);
+
+            if prefix == "+" {
+                has_positive_requirement = true;
+                if !text.contains(keyword) {
+                    is_match = false;
+                    break;
+                }
+            } else if prefix == "-" {
+                has_negative_requirement = true;
+                if text.contains(keyword) {
+                    is_match = false;
+                    break;
+                }
+            } else {
+                if !text.contains(keyword) {
+                    is_match = false;
+                    break;
+                }
+            }
+        }
+
+        if is_match && (!has_positive_requirement || has_negative_requirement) {
             println!("{}", text);
         }
     }
 
     Ok(())
+}
+
+fn parse_keyword(keyword: &str) -> (&str, &str) {
+    if keyword.starts_with('+') {
+        ("+", &keyword[1..])
+    } else if keyword.starts_with('-') {
+        ("-", &keyword[1..])
+    } else {
+        ("", keyword)
+    }
 }
