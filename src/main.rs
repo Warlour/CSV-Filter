2cb3529dfa1758ba2c92840cbfad4814282070bb
Mathias K. E. Sørensen <68961022+Warlour@users.noreply.github.com>
2023-05-27 01:10:32 +0200

    Removed plus prefix only kept minus prefix

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,8 @@ fn main() {
             .takes_value(true))
         .get_matches();
 
-    let filename = matches.value_of("input").unwrap();
-    let keywords = matches.value_of("keywords").unwrap();
+    let filename: &str = matches.value_of("input").unwrap();
+    let keywords: &str = matches.value_of("keywords").unwrap();
 
     if let Err(e) = read_csv_file(filename, keywords) {
         println!("Error: {}", e);
@@ -42,19 +42,13 @@ fn read_csv_file(filename: &str, keywords: &str) -> Result<(), Box<dyn Error>> {
         let text: &str = record.get(0).unwrap_or("");
 
         let mut is_match: bool = true;
-        let mut has_positive_requirement: bool = false;
+        let has_positive_requirement: bool = false;
         let mut has_negative_requirement: bool = false;
 
         for keyword in &keyword_list {
             let (prefix, keyword) = parse_keyword(keyword);
 
-            if prefix == "+" {
-                has_positive_requirement = true;
-                if !text.contains(keyword) {
-                    is_match = false;
-                    break;
-                }
-            } else if prefix == "-" {
+            if prefix == "-" {
                 has_negative_requirement = true;
                 if text.contains(keyword) {
                     is_match = false;
@@ -77,8 +71,8 @@ fn read_csv_file(filename: &str, keywords: &str) -> Result<(), Box<dyn Error>> {
 }
 
 fn parse_keyword(keyword: &str) -> (&str, &str) {
-    if keyword.starts_with('+') {
-        ("+", &keyword[1..])
+    if keyword.starts_with('\\') {
+        ("", &keyword[1..])
     } else if keyword.starts_with('-') {
         ("-", &keyword[1..])
     } else {
